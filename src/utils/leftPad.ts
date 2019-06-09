059b0b9ddb1c5f95ab37dca85635e60f3baf0d60
Benjamin S. Leveritt <benjamin@nc-squared.com>
2019-06-09 12:42:17 +0100

    Added support for overflow to `leftPad`

--- src/utils/leftPad.ts
+++ src/utils/leftPad.ts
@@ -4,4 +4,7 @@
  * @param num Number to be padded
  * @returns {string}
  */
-export const leftPad = (num: number): string => ('000' + num).slice(-3);
+export const leftPad = (num: number | string): string =>
+  ('000' + num).slice(
+    num.toString().length > 3 ? num.toString().length - 1 : -3
+  );
